aebb0d73ff8e85ef099f552d043e556c8a83ffe7
unknown <silverf@yandex.ru>
2025-01-07 21:43:38 +0700

    delete shift reg  delay_vld

--- 04_arithmetics_and_pipelining/04_10_11_distributor/04_10_11_distributor.sv
+++ 04_arithmetics_and_pipelining/04_10_11_distributor/04_10_11_distributor.sv
@@ -32,28 +32,24 @@ module distributor
 
     logic [LATENCY-1:0]            ptr_in;                                    // one-hot registre
     logic [$clog2(LATENCY-1):0]    n_out;                                     // регистр хранения номера вычислителя с валидным результатом  
-    logic [LATENCY-1 :0]           delay_vld;                                 // сдвиговый регистр для сигнала vld 
-    logic [FLEN - 1:0]             res_0, res_1, res_2, res_3, res_4, res_5, res_6, res_7, res_8, res_9;
-    logic                          start_0, start_1, start_2, start_3, start_4, start_5, start_6, start_7, start_8, start_9;    
-
-    logic [LATENCY-1:0]            res_vld_o, res_negative_o, err_o, busy_o;    // векторы для подключения выходных сигналов вычислителей 
+ 
+    logic [LATENCY-1:0]            res_vld_o, res_negative_o, err_o, busy_o;  // векторы для подключения выходных сигналов вычислителей 
     logic [FLEN - 1:0]             res_o [LATENCY-1:0];
 
     always_ff @ (posedge clk) begin
       if (rst) begin
         ptr_in    <= 1;                                                      // загружаем "1" в one-hot регистр селектор              
         n_out     <='0;                                                      // "0"  в регистр-селектор выходов вычислителей
-        delay_vld <='0;  
+ 
       end
       else begin
-        delay_vld <= {delay_vld[LATENCY-1:0],arg_vld};                       // передаем данные arg_vld с задержкой в LATENCY тактов на выход модуля
-        if (arg_vld) begin 
+        if (arg_vld) 
           ptr_in    <= {ptr_in[LATENCY-2:0],ptr_in[LATENCY-1]};              // пришли данные - сдвигаем входной регистр-селектор маски
-        end
-       
-        if (delay_vld[LATENCY-1]) begin                                      // если на выходе готовы данные то   
+        
+                                       
+        if (|res_vld_o) begin                                                // если на выходе готовы данные то
           if (n_out == (LATENCY-1 )) n_out <= '0;                            // и если дошли до последнего вычислителя то сбрасываем номер вычислителя    
-          else                      n_out <= n_out + 1 ;                     // иначе увеличиваем номер вычислителя подключенного к выходу
+          else                       n_out <= n_out + 1 ;                     // иначе увеличиваем номер вычислителя подключенного к выходу
         end
           
       end 
@@ -89,7 +85,7 @@ module distributor
     res          = res_o[n_out]  ; 
     res_negative = res_negative_o[n_out];
     err          = err_o[n_out];
-    res_vld      = delay_vld[LATENCY-1];
+    res_vld      = res_vld_o[n_out];   
   end
 
 
